0fe23bf2982f8c23790b1f0fa6e97e90dcc91a9c
ChesterTsai <tsaichester71@gmail.com>
2024-11-24 09:22:17 +0800

    realign the nestings, made loop checks more frequently, and made the cooldown runs after every processes are done

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# All the comments written in this file haven't been re-write.
-# Just look at the codes, not the comments
-# I don't wanna give any fucks about the comments
-
 # https://discordapp.com/api/oauth2/authorize?client_id=1248612928601722971&permissions=8&scope=bot
 
 import json
@@ -24,54 +20,54 @@ async def on_ready():
     print("Bot Now Online!")
     checkforvideos.start()
 
-@tasks.loop(seconds=30)
+@tasks.loop(seconds=2)
 async def checkforvideos():
-  with open(file_location, "r") as f:
-    data=json.load(f)
-  for youtube_channel in data:
-    channel = f"https://www.youtube.com/channel/{youtube_channel}"
+    with open(file_location, "r") as f:
+        data=json.load(f)
+    for youtube_channel in data:
+        channel = f"https://www.youtube.com/channel/{youtube_channel}"
     
     videos = requests.get(channel+"/videos").text
     shorts = requests.get(channel+"/shorts").text
     
     try:
-      latest_video_url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', videos).group()
-      latest_shorts_url = "https://www.youtube.com/shorts/" + re.search('(?<="videoId":").*?(?=")', shorts).group()
+        latest_video_url = "https://www.youtube.com/watch?v=" + re.search('(?<="videoId":").*?(?=")', videos).group()
+        latest_shorts_url = "https://www.youtube.com/shorts/" + re.search('(?<="videoId":").*?(?=")', shorts).group()
     except:
-      continue
+        continue
     
     # New Video Mentioning
     if not str(data[youtube_channel]["latest_video_url"]) == latest_video_url:
 
-      data[str(youtube_channel)]['latest_video_url'] = latest_video_url
+        data[str(youtube_channel)]['latest_video_url'] = latest_video_url
 
-      with open(file_location, "w") as f:
-        json.dump(data, f)
+        with open(file_location, "w") as f:
+            json.dump(data, f)
 
-      discord_channel_id = data[str(youtube_channel)]['notifying_discord_channel']
-      discord_channel = bot.get_channel(int(discord_channel_id))
+        discord_channel_id = data[str(youtube_channel)]['notifying_discord_channel']
+        discord_channel = bot.get_channel(int(discord_channel_id))
 
-      msg = f"@everyone\n{latest_video_url}"
+        msg = f"@everyone\n{latest_video_url}"
 
-      await discord_channel.send(msg)
-      print(f'[{datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")} INFO] New Video Info Sent!')
-      time.sleep(300)
+        await discord_channel.send(msg)
+        print(f'[{datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")} INFO] New Video Info Sent!')
     
     # New Shorts Mentioning
     if not str(data[youtube_channel]["latest_shorts_url"]) == latest_shorts_url:
 
-      data[str(youtube_channel)]['latest_shorts_url'] = latest_shorts_url
+        data[str(youtube_channel)]['latest_shorts_url'] = latest_shorts_url
 
-      with open(file_location, "w") as f:
-        json.dump(data, f)
+        with open(file_location, "w") as f:
+            json.dump(data, f)
 
-      discord_channel_id = data[str(youtube_channel)]['notifying_discord_channel']
-      discord_channel = bot.get_channel(int(discord_channel_id))
+        discord_channel_id = data[str(youtube_channel)]['notifying_discord_channel']
+        discord_channel = bot.get_channel(int(discord_channel_id))
 
-      msg = f"@everyone\n{latest_shorts_url}"
+        msg = f"@everyone\n{latest_shorts_url}"
 
-      await discord_channel.send(msg)
-      print(f'[{datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")} INFO] New Shorts Info Sent!')
-      time.sleep(300)
+        await discord_channel.send(msg)
+        print(f'[{datetime.datetime.now().strftime("%Y/%m/%d, %H:%M:%S")} INFO] New Shorts Info Sent!')
+    
+    time.sleep(300)
 
 bot.run(TOKEN, log_handler=None)
